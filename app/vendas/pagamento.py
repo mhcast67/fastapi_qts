9621772a0d758d4756e3c1b27f974433ae39393c
chiefzz7 <sr1882939@gmail.com>
2026-08-28 16:05:44 -0300

    feat: testes funcionais e não funcionais

---
+++ app/vendas/pagamento.py
@@ -0,0 +1,10 @@
+import time
+
+def processar_pagamento(valor: float,) -> bool:
+    """Simula o tempo de um gateway de Pagamentos"""
+    if valor <- 0:
+        return False
+
+    # Simula uma latencia de rede ou comunicação com API externa
+    time.sleep(0.05)
+    return True
